c99c80e564b3a39960d880beaff2fd5a43fb12d9
Neyvia <a20130003@utem.edu.mx>
2016-07-08 17:15:57 -0500

    vista de usuarios
    se agrego una vista y los botones

--- application/controllers/usuario.php
+++ application/controllers/usuario.php
@@ -3,6 +3,7 @@ class Usuario extends CI_controller{
 
 	function index(){
 	$this->load->view('usuario/index');
+	$this->load->view('usuario/index2');
 	}
 }
 ?>
---
+++ application/views/usuario/index2.php
@@ -0,0 +1,69 @@
+
+<body>
+
+    <!-- Full Width Image Header with Logo -->
+    <!-- Image backgrounds are set within the full-width-pics.css file. -->
+    
+
+  <div class="container">
+        <div id="carousel">
+            <figure><img src="http://lorempixel.com/186/116/nature/1" alt=""></figure>
+            <figure><img src="http://lorempixel.com/186/116/nature/2" alt=""></figure>
+            <figure><img src="http://lorempixel.com/186/116/nature/3" alt=""></figure>
+            <figure><img src="http://lorempixel.com/186/116/nature/4" alt=""></figure>
+            <figure><img src="http://lorempixel.com/186/116/nature/5" alt=""></figure>
+            <figure><img src="http://lorempixel.com/186/116/nature/6" alt=""></figure>
+            <figure><img src="http://lorempixel.com/186/116/nature/7" alt=""></figure>
+            <figure><img src="http://lorempixel.com/186/116/nature/8" alt=""></figure>
+            <figure><img src="http://lorempixel.com/186/116/people/9" alt=""></figure>
+        </div>
+    </div>
+
+ <div class="row">
+    <center>
+
+        <a class="btn btn-default loading">Default</a>
+    <a class="btn btn-primary loading">Primary</a>
+    <a class="btn btn-danger loading">Danger</a>
+    <a class="btn btn-success loading">Success</a>
+    <a class="btn btn-info loading">Info</a>
+    <a class="btn btn-warning loading">Warning</a>
+</center>
+       </div>
+
+
+
+
+   
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+</div>
+
+    <!-- Fixed Height Image Aside -->
+    <!-- Image backgrounds are set within the full-width-pics.css file. -->
+   
+
+    <!-- Content Section -->
+    
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
