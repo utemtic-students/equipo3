7a9067c721993c17f2050ab371fdebe40fa72de0
Neyvia <a20130003@utem.edu.mx>
2016-07-11 21:14:57 -0500

    botones
    cambio de nombre de botones

--- application/views/usuario/index2.php
+++ application/views/usuario/index2.php
@@ -13,36 +13,47 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Mathieu Richard</a>
+          <a class="navbar-brand" href="#">NEYVIA KAHERY</a>
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li><a href="#accueil">Home</a></li>
-            <li><a href="#gallery">Gallery</a></li>
-            <li><a href="#contact">Contact</a></li>
+          
           </ul>
         </div>
     </div>
+
 <div id="accueil">
-  <h1>Hello,</h1>
-  <h2>Mathieu Richard</h2>
+
+  <h1>BIENVENIDA, NEYVIA KAHERY</h1>
+  <h2></h2>
+  <p>
+<br>
+<br>
+<br>
+<br>
+
+ <div class="row">
+    <center>
+
+        <a class="btn btn-default loading">A/C</a>
+    <a class="btn btn-primary loading">LuzExterior</a>
+    <a class="btn btn-danger loading">LuzInterior</a>
+    <a class="btn btn-success loading">Ventanas</a>
+    <a class="btn btn-info loading">Garage</a>
+    <a class="btn btn-warning loading">Riego</a>
+</center>
+       </div>
+
+
+
+
 </div>
+
 
 
     
        
 
- <div class="row">
-    <center>
-
-        <a class="btn btn-default loading">Default</a>
-    <a class="btn btn-primary loading">Primary</a>
-    <a class="btn btn-danger loading">Danger</a>
-    <a class="btn btn-success loading">Success</a>
-    <a class="btn btn-info loading">Info</a>
-    <a class="btn btn-warning loading">Warning</a>
-</center>
-       </div>
 
 
 
